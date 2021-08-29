ace572674989d30d29c0eedebebef34828b554b1
David Pacheco <54561496+David-Hdez@users.noreply.github.com>
2021-08-28 20:10:56 -0500

    Registro de usuarios
    Crear servicios de  usuarios

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
   public page_title: string
   public user: User
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.page_title = 'Registrate'
     this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '')
   }
 
   ngOnInit(): void {
     console.log('Compnente de registro cargado')
+    console.log(this._userService.test())
   }
 
   register(form: any) {
