c270505427c7fdc4fdf6048c6638a898637a58bd
David Pacheco <54561496+David-Hdez@users.noreply.github.com>
2021-08-28 19:47:37 -0500

    Registro de usuarios (web SPA con Angular)
    Formulario de registro con Angular

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'register',
@@ -7,13 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   public page_title: string
+  public user: User
 
   constructor() {
     this.page_title = 'Registrate'
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '')
   }
 
   ngOnInit(): void {
     console.log('Compnente de registro cargado')
+  }
+
+  register(form: any) {
+    console.debug(form)
+    console.debug(this.user)
   }
 
 }
