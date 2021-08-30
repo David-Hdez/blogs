a2f31faa2798cdc594aa7b1039b627654758abc2
David Pacheco <54561496+David-Hdez@users.noreply.github.com>
2021-08-29 20:20:38 -0500

    Login de usuarios
    Identificar al usuario

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,19 +12,50 @@ import { UserService } from '../../services/user.service';
 export class LoginComponent implements OnInit {
   public page_title: string
   public user: User
+  public status: string
+  public jwt: string
+  public identity: string
 
   constructor(
     private _userService: UserService
   ) {
     this.page_title = 'Identifícate'
     this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '')
+    this.status = ''
+    this.jwt = ''
+    this.identity = ''
   }
 
   ngOnInit(): void {
   }
 
   login(form: any) {
-    console.debug(this.user)
+    this._userService.signup(this.user).subscribe(
+      response => {
+        if (response.status != 'error') {
+          this.status = 'success'
+          this.jwt = response
+
+          this._userService.signup(this.user, true).subscribe(
+            response => {
+              this.identity = response
+              console.debug('jwt', this.jwt)
+              console.debug('identity', this.identity)
+            },
+            error => {
+              this.status = 'error'
+              console.error('Identity', <any>error)
+            }
+          )
+        } else {
+          this.status = 'error'
+        }
+      },
+      error => {
+        this.status = 'error'
+        console.error('Token', <any>error)
+      }
+    )
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -16,7 +16,7 @@ export class UserService {
 
   /**
    * Registro de usuario   
-   * @param user Datos del usuarios
+   * @param user Datos del usuario
    * @returns {Observable} Respuesta del API
    */
   register(user: User): Observable<any> {
@@ -26,5 +26,22 @@ export class UserService {
     let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
 
     return this._http.post(this.urlApi + 'user', body_form_data, { headers: headers })
+  }
+
+  /**
+   * Login de usuario   
+   * @param user Datos del usuario
+   * @returns {Observable} Respuesta del API
+   */
+  signup(user: any, getToken: any = null): Observable<any> {
+    if (getToken != null) {
+      user.getToken = 'true'
+    }
+
+    let user_data = JSON.stringify(user)
+    let body_form_data = 'user=' + user_data
+    let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+
+    return this._http.post(this.urlApi + 'user/login', body_form_data, { headers: headers })
   }
 }
