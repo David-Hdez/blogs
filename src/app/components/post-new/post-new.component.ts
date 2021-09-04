4b45507441be04237a7cbc1db034b845bc1c2861
David Pacheco <54561496+David-Hdez@users.noreply.github.com>
2021-09-04 13:48:54 -0500

    Componente Entradas

--- src/app/components/post-new/post-new.component.ts
+++ src/app/components/post-new/post-new.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { CategoryService } from '../../services//category.service';
+import { Post } from '../../models/post';
 
 @Component({
   selector: 'app-post-new',
   templateUrl: './post-new.component.html',
-  styleUrls: ['./post-new.component.css']
+  styleUrls: ['./post-new.component.css'],
+  providers: [UserService, CategoryService]
 })
 export class PostNewComponent implements OnInit {
   public page_title: string
+  public identity: any
+  public jwt: any
+  public post: Post
 
-  constructor() {
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _userService: UserService,
+    private _categoryService: CategoryService
+  ) {
     this.page_title = 'Crear una entrada'
+    this.identity = this._userService.getIdentity()
+    this.jwt = this._userService.getToken()
+    this.post = new Post(1, this.identity.sub, 1, '', '', '', null)
   }
 
   ngOnInit(): void {
