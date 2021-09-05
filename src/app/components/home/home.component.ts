1141a8b7290b94e5b23c627bb914fb8be9a58753
David Pacheco <54561496+David-Hdez@users.noreply.github.com>
2021-09-04 20:56:46 -0500

    Listado de entradas
    Botones a usuarios identificados y dueños del post.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,24 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from '../../models/post';
 import { PostService } from '../../services/post.service';
 import { global } from '../../services/global';
+import { UserService } from '../..//services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [PostService]
+  providers: [PostService, UserService]
 })
 export class HomeComponent implements OnInit {
   public page_title: string
   public urlApi: string
   public posts: Array<Post>
+  public identity: any
+  public jwt: any
 
   constructor(
-    private _postService: PostService
+    private _postService: PostService,
+    private _userService: UserService
   ) {
     this.page_title = 'Inicio'
     this.urlApi = global.urlApi
     this.posts = []
+    this.identity = this._userService.getIdentity()
+    this.jwt = this._userService.getToken()
   }
 
   ngOnInit(): void {
