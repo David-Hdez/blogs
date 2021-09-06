ff86e931c42eeea486a930d49572b5b8dfbd8c39
David Pacheco <54561496+David-Hdez@users.noreply.github.com>
2021-09-05 19:26:03 -0500

    Entradas y categorías
    Detalle de categoría

--- src/app/components/category-detail/category-detail.component.ts
+++ src/app/components/category-detail/category-detail.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Category } from '../../models/category';
+import { Post } from '../../models/post';
+import { CategoryService } from '../../services/category.service';
+import { global } from '../../services/global';
 
 @Component({
   selector: 'app-category-detail',
   templateUrl: './category-detail.component.html',
-  styleUrls: ['./category-detail.component.css']
+  styleUrls: ['./category-detail.component.css'],
+  providers: [CategoryService]
 })
 export class CategoryDetailComponent implements OnInit {
+  public title: string
+  public category: Category
+  public posts: Post
+  public urlApi: string
 
-  constructor() { }
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _categoryService: CategoryService
+  ) {
+    this.title = 'Mostrando por categoría'
+    this.category = <Category>{}
+    this.posts = <Post>{}
+    this.urlApi = global.urlApi
+
+  }
 
   ngOnInit(): void {
+    this.getPostsByCategory()
+  }
+
+  /**
+   * Charging the ID of category from URL param
+   */
+  getPostsByCategory() {
+    this._route.params.subscribe(params => {
+      let category_id = +params['id'] // Convert to number    
+
+      this._categoryService.show(category_id).subscribe(
+        response => {
+          if (response.code == 200) {
+            this.category = response.category
+          } else {
+            this._router.navigate(['/inicio'])
+          }
+        },
+        error => {
+          console.error(<any>error)
+          this._router.navigate(['/inicio'])
+        }
+      )
+    })
   }
 
 }
